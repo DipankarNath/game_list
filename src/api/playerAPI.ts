a22c84944920264fee63dd09afbfda788254d2ad
Dipankar Nath <dipankarnath008@gmail.com>
2024-05-29 08:01:47 +0530

    add external api url

--- src/api/playerAPI.ts
+++ src/api/playerAPI.ts
@@ -1,8 +1,11 @@
-import { PlayerType } from "../containers/dashboard/types";
+import {PlayerType} from "../containers/dashboard/types";
 
 export function addNewPlayer(player: PlayerType) {
-  return new Promise<{ data: PlayerType }>((resolve) => {
-    console.log('player added');
-    setTimeout(() => resolve({ data: player }), 500);
-  });
+    return new Promise<{ data: PlayerType }>((resolve) => {
+        console.log('player added');
+        setTimeout(() => resolve({data: player}), 500);
+    });
 }
+
+// external API to fetch players data
+// https://my-json-server.typicode.com/cb-dipankarnath/dataForTask/playerList
